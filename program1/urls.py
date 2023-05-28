00248b64f5d88408341add516e0c6b3f40d6b46d
tejas1505 <93146002+tejas1505@users.noreply.github.com>
2023-05-28 20:53:53 +0530

    Update urls.py

--- program1/urls.py
+++ program1/urls.py
@@ -20,8 +20,14 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from sensor import views
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sensor.urls')),
-    path('', include('pwa.urls'))
+    path('', include('pwa.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
