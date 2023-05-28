2ee345911fd20625cf5772c6059beb42ce18c7f9
tejas1505 <93146002+tejas1505@users.noreply.github.com>
2023-05-28 23:02:34 +0530

    Update settings.py

--- program1/settings.py
+++ program1/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 # PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'sensor/templates/','sw.js')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
