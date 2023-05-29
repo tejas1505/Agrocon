da6075e4d2641f136c38684563c85838adadecee
tejas1505 <93146002+tejas1505@users.noreply.github.com>
2023-05-29 22:36:42 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
  echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
- pip install sqlite3
+ pip install db-sqlite3
  python3.9 manage.py collectstatic --noinput --clear
  python3.9 manage.py makemigrations
  python3.9 manage.py migrate
