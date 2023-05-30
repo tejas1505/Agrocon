5f657c4e5afa6905c70070ba1436ecf6a9f4009c
tejas1505 <93146002+tejas1505@users.noreply.github.com>
2023-05-30 20:15:42 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
  echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
+ python3.9 manage.py createsuperuser
  python3.9 manage.py makemigrations
  python3.9 manage.py migrate
  echo "BUILD END"
