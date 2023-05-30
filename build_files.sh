838a5e659a808bc1f4affbd6a2e5a9d67f4d1fe3
tejas1505 <93146002+tejas1505@users.noreply.github.com>
2023-05-30 20:21:02 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
  echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
- python3.9 manage.py createsuperuser
+ echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('apcoer', '', 'apcoer@123')" | python manage.py shell
  python3.9 manage.py makemigrations
  python3.9 manage.py migrate
  echo "BUILD END"
