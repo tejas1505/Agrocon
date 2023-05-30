d29c875b8fa949ba4129717db5927d88b68d5c7a
tejas1505 <93146002+tejas1505@users.noreply.github.com>
2023-05-30 21:10:48 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,4 @@
  python3.9 manage.py collectstatic --noinput --clear
  python3.9 manage.py makemigrations
  python3.9 manage.py migrate
- python3.9 manage.py createsuperuser --username=apcoer --password=apcoer@123 --noinput
  echo "BUILD END"
